feb0246bb2ad73a8bb58e5d7a806b6fa6c94fc65
yitianshao <ytshao@is.mpg.de>
2022-01-20 18:09:29 +0100

    Before removing the obsoleted PouchModelGenerator

--- PouchModelAnalysis/SearchValidArc.py
+++ PouchModelAnalysis/SearchValidArc.py
@@ -19,25 +19,25 @@ epsilon_s = 2.0 # Relative permittivity of the shell
 epsilon_f = 3.2 # Relative permittivity of the fluid
 
 ''' Match arc length '''
-RSearchNum = 10000
+RSearchNum = 100000
 
 time0 = time.time()
-pouchStructure = np.array([6, 5, 4, 3, 2, 1])
-rectNum = np.sum(pouchStructure - 1)  # Number of rectangle pouch-cell
-triNum = rectNum * 2 - len(pouchStructure) + 1  # Number of triangle pouch-cell
+# pouchStructure = np.array([6, 5, 4, 3, 2, 1])
+# rectNum = np.sum(pouchStructure - 1)  # Number of rectangle pouch-cell
+# triNum = rectNum * 2 - len(pouchStructure) + 1  # Number of triangle pouch-cell
 
-mRange = np.arange(4.7, 5.0, 0.001)
-RRange = np.logspace(np.log10(8000), np.log10(8), RSearchNum)
+mRange = np.arange(4.5, 5.0, 0.001)
+RRange = np.logspace(np.log10(50000), np.log10(7), RSearchNum)
 
 with open('ValidArcLength.csv', 'w', newline='') as csvFile:
     writer = csv.writer(csvFile)
-    writer.writerow(('dashHalfDist', 'dashSpace', 'R', 'triArc', 'triVol', 'rectVol', 'totalVol'))
+    writer.writerow(('dashHalfDist', 'dashSpace', 'R', 'triArc', 'triVol', 'rectVol', 'dashLength'))
 
     for c in [12.66, 8.66, 4.66]:
         for m in mRange:
             print("\n\n[%.0f s] c = %.6f mm, m = %.6f mm" % (time.time() - time0, c, m))
 
-            unFound = True;
+            unFound = True
             i = 0
             while (unFound and i < RSearchNum):
                 R = RRange[i]
@@ -48,15 +48,25 @@ with open('ValidArcLength.csv', 'w', newline='') as csvFile:
 
                     triVol = triPouch0.getVolume(intStepSize)
 
-                    rectPouch0 = RectanglePouch(triPouch0.r, 12, triPouch0.m)
+                    w = 24.66 - c
+                    rectPouch0 = RectanglePouch(triPouch0.r, w, triPouch0.m)
                     rectVol = rectPouch0.getVolume()
 
-                    totalVol = (triNum * triVol + rectNum * rectVol)
+                    print("\n\n[%.0f s] c = %.6f mm, m = %.6f mm, triArc = %.6f mm, R = %.6f mm, w = %.6f mm" %
+                          (time.time() - time0, c, m, triArc, R, w))
 
-                    print("\n\n[%.0f s] c = %.6f mm, m = %.6f mm, triArc = %.6f mm, R = %.6f mm, totalVol = %.2f mL" %
-                          (time.time() - time0, c, m, triArc, R, totalVol / 1000))
+                    writer.writerow([triPouch0.m, triPouch0.c, triPouch0.R, triArc, triVol, rectVol, w])
 
-                    writer.writerow([triPouch0.m, triPouch0.c, triPouch0.R, triArc, triVol, rectVol, totalVol])
+                    if c == 8.66: # Special design when c = 8.66mm, w = 20mm
+                        w = 20
+                        rectPouch0 = RectanglePouch(triPouch0.r, w, triPouch0.m)
+                        rectVol = rectPouch0.getVolume()
+
+                        print("\n\n[%.0f s] c = %.6f mm, m = %.6f mm, triArc = %.6f mm, R = %.6f mm, w = %.6f mm" %
+                              (time.time() - time0, c, m, triArc, R, w))
+
+                        writer.writerow([triPouch0.m, triPouch0.c, triPouch0.R, triArc, triVol, rectVol, w])
+
 
                     unFound = False
                 else:
